82573623ca7b68af6346b4a198843187d86f850a
esharif20 <eshansharif2004@gmail.com>
2025-09-24 17:14:17 +0100

    restructred files layout

--- src/ingesting_data/ingest.py
+++ src/ingesting_data/ingest.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import pandas as pd
 
@@ -11,8 +12,12 @@ def fetch_players() -> pd.DataFrame:
     players = pd.DataFrame(data["elements"])
     return players[["id", "first_name", "second_name", "team", "now_cost", "total_points"]]
 
+import os
+
 if __name__ == "__main__":
     df = fetch_players()
-    os.makedirs("fpl_data", exist_ok=True)  # create folder if missing
-    df.to_csv("fpl_data/players.csv", index=False)
-    print(f"Saved {len(df)} players to data/players.csv")
+    data_dir = os.path.join("src", "fpl_data", "raw")
+    os.makedirs(data_dir, exist_ok=True)
+    out_path = os.path.join(data_dir, "players.csv")
+    df.to_csv(out_path, index=False)
+    print(f"Saved {len(df)} players to {out_path}")
